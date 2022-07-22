57ee23244d88f3a9b9bfa8e61b61be009303f923
sdauuse <751648872@qq.com>
2022-07-22 13:45:11 +0800

    完成移动端收货地址功能

--- idea/take_out/src/main/java/com/miao/controller/SetmealController.java
+++ idea/take_out/src/main/java/com/miao/controller/SetmealController.java
@@ -92,4 +92,18 @@ public class SetmealController {
         setmealService.removeWithDish(ids);
         return R.success("删除成功");
     }
+
+
+    @GetMapping("/list")
+    public R<List<Setmeal>> list(Setmeal setmeal) {
+
+        LambdaQueryWrapper<Setmeal> queryWrapper = new LambdaQueryWrapper<>();
+        queryWrapper.eq(setmeal.getStatus() != null, Setmeal::getStatus, setmeal.getStatus());
+        queryWrapper.eq(setmeal.getCategoryId() != null, Setmeal::getCategoryId, setmeal.getCategoryId());
+        queryWrapper.orderByDesc(Setmeal::getUpdateTime);
+
+        List<Setmeal> list = setmealService.list(queryWrapper);
+
+        return R.success(list);
+    }
 }
---
+++ idea/take_out/src/main/java/com/miao/service/AddressBookService.java
@@ -0,0 +1,8 @@
+package com.miao.service;
+
+import com.baomidou.mybatisplus.extension.service.IService;
+import com.miao.entity.AddressBook;
+
+public interface AddressBookService extends IService<AddressBook> {
+
+}
