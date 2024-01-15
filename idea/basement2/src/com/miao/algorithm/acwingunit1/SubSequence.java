03b0d0829372a20baa39daf9eabcc8af3cae0959
sdauuse <751648872@qq.com>
2024-01-15 12:37:12 +0800

    力扣+acwing

--- idea/basement2/src/com/miao/algorithm/acwingunit1/SubSequence.java
+++ idea/basement2/src/com/miao/algorithm/acwingunit1/SubSequence.java
@@ -1,8 +1,50 @@
 package com.miao.algorithm.acwingunit1;
 
-public class SubSequence {
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Scanner;
 
-    public static void main(String[] args) {
-        
+public class SubSequence {
+    public static int N = (int) (1e5 + 10);
+    public static int[] a = new int[N];
+    public static int[] b = new int[N];
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
+        String[] s = bf.readLine().split(" ");
+        int n = Integer.parseInt(s[0]);
+        int m = Integer.parseInt(s[1]);
+
+        String[] s1 = bf.readLine().split(" ");
+        String[] s2 = bf.readLine().split(" ");
+
+        for (int i = 0; i < n; i++) {
+            a[i] = Integer.parseInt(s1[i]);
+        }
+        for (int i = 0; i < m; i++) {
+            b[i] = Integer.parseInt(s2[i]);
+        }
+
+        twoPoint(n, m);
+    }
+
+    private static void twoPoint(int n, int m) {
+
+        int l = 0;
+        int r = 0;
+        while (l < n && r < m) {
+
+            if (a[l] == b[r]) {
+                l++;
+            }
+            r++;
+        }
+
+        if (l >= n) {
+            System.out.println("Yes");
+        } else {
+            System.out.println("No");
+        }
     }
 }
